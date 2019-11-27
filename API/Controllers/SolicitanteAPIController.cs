5ad4fb9bd9958394b4d6eb3ee024a89cfc77f579
Luis Machado <luislocio@gmail.com>
2019-11-26 21:07:42 -0300

    Criação do Controller do Solicitante

---
+++ API/Controllers/SolicitanteAPIController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repository.DAL;
+
+namespace API.Controllers
+{
+    [Route("api/solicitante")]
+    [ApiController]
+    public class SolicitanteAPIController : ControllerBase
+    {
+        private readonly SolicitanteDAO _solicitanteDAO;
+
+        public SolicitanteAPIController(SolicitanteDAO solicitanteDAO)
+        {
+            _solicitanteDAO = solicitanteDAO;
+        }
+
+        // GET: /api/solicitante/all
+        [HttpGet]
+        [Route("all")]
+        public IActionResult ListarTodos()
+        {
+            return Ok(_solicitanteDAO.ListarSolicitantes());
+        }
+    }
+}
